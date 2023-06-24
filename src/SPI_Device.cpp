c36f700e6659d07ea869b8cba59526f221358a37
NextGenXR <jenni@nextgenxr.com>
2023-06-24 13:11:19 -0500

    Update descriptions for Public repo

---
+++ src/SPI_Device.cpp
@@ -0,0 +1,18 @@
+/*
+ * SPIAPI.cpp
+ *
+ *  Created on: Jun 20, 2023
+ *      Author: joconnor
+ */
+
+#include "SPI_Device.h"
+
+SPI_API::SPI_API() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SPI_API::~SPI_API() {
+	// TODO Auto-generated destructor stub
+}
+
