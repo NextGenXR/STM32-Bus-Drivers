c08ca8f812942a6320ee1f14e412289d13dcf395
Jenni O'Connor <jenni@nextgenxr.com>
2023-07-06 21:21:36 -0500

    Initial class for SPI controller instance

---
+++ inc/SPI_Instance.h
@@ -0,0 +1,17 @@
+/*
+ * SPI_Instance.h
+ *
+ *  Created on: Jul 6, 2023
+ *      Author: joconnor
+ */
+
+#ifndef DRIVERS_BSP_STM32_BUS_DRIVERS_SPI_INSTANCE_H_
+#define DRIVERS_BSP_STM32_BUS_DRIVERS_SPI_INSTANCE_H_
+
+class SPI_Instance {
+public:
+	SPI_Instance();
+	virtual ~SPI_Instance();
+};
+
+#endif /* DRIVERS_BSP_STM32_BUS_DRIVERS_SPI_INSTANCE_H_ */
---
+++ src/SPI_Instance.cpp
@@ -0,0 +1,18 @@
+/*
+ * SPI_Instance.cpp
+ *
+ *  Created on: Jul 6, 2023
+ *      Author: joconnor
+ */
+
+#include "SPI_Instance.h"
+
+SPI_Instance::SPI_Instance() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SPI_Instance::~SPI_Instance() {
+	// TODO Auto-generated destructor stub
+}
+
