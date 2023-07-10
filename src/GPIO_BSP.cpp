759a679133defb65da2a59812716313809865bfc
Jenni O'Connor <jenni@nextgenxr.com>
2023-07-10 08:27:44 -0500

    Add guard statements

--- src/GPIO_BSP.cpp
+++ src/GPIO_BSP.cpp
@@ -14,36 +14,74 @@
 #include <Analog_IO.h>
 #include <Digital_IO.h>
 
-Analog_IO* aio;
-Digital_IO* dio;
+static Analog_IO* aio = nullptr;
+static Digital_IO* dio = nullptr;
+bool begun = false;
+
+#define MAX_PINS 128
+PinMode_TypeDef pinStates[MAX_PINS];
 
 void begin()
 {
-	aio = Analog_IO::getInstance();
-	dio = Digital_IO::getInstance();
+	if(begun == false) {
+		aio = Analog_IO::getInstance();
+		dio = Digital_IO::getInstance();
 
+		// TODO: Further initialization
+		for(auto count = 0; count < MAX_PINS; count++) {
+			pinStates[count] = PinMode_TypeDef::NONE;
+		}
+
+		begun = true;
+	}
 
 }
 
 
 bool digitalRead(uint8_t pin) {
+	if(begun != true) {
+		begin();
+	}
+
+	// TODO: digitalRead
 	return true;
+
 }
 
 bool digitalWrite(uint8_t pin, bool value) {
+	if(begun != true) {
+		begin();
+	}
+
+	// TODO: digitalWrite
 	return true;
 }
 
 void digital_Write(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {
+	if(begun != true) {
+		begin();
+	}
+
+	// TODO: digital_Write
 
 }
 
-bool pinMode(uint8_t pin, PIN_MODE mode) {
+bool pinMode(uint8_t pin, PinMode_TypeDef mode) {
+	if(begun != true) {
+		begin();
+	}
+
+	// TODO: pinMode
 	return true;
 }
 
 
 bool pin_Mode(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {
+	if(begun != true) {
+		begin();
+	}
+
+	// TODO: pin_Mode
 	return true;
 }
 
